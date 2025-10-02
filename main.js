12e6a374e475daa3e72a920428ec9cd2b5220616
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-10-02 10:34:19 -0400

    Add orbit and basic lighting

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const showHelpers = true;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -7,22 +10,37 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.SphereGeometry(2, 16, 12);
+const material = new THREE.MeshStandardMaterial({ color: 0xff7518 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
-camera.position.z = 5;
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(2.5, 2.5, 0);
+scene.add(pointLight);
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+if (showHelpers) {
+  const lightHelper = new THREE.PointLightHelper(pointLight);
+  const gridHelper = new THREE.GridHelper(200, 50);
+  scene.add(lightHelper, gridHelper);
 }
 
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  sphere.rotation.y += 0.01;
+
+  controls.update();
+  renderer.render(scene, camera);
+}
